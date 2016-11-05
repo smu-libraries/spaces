433c527bbd8302efe03842f8146901aba2c6948d
Ler Wee Hiong <hum.ler@outlook.com>
2016-11-05 21:08:26 +0800

    Workaround bug between command-line-args and gh-pages

--- tasks/publish_github.js
+++ tasks/publish_github.js
@@ -1,13 +1,12 @@
 /**
  * @file Publishes the built site to GitHub Project Pages.
  *
- * Usage: node publish_github.js --publish|-p <folder>
+ * Usage: node publish_github.js (--publish|-p) <folder>
  *
  * There is no error checking -- script fails at the first error. Stick to lowercase file names and extensions. Use UTF-8 file encoding.
  */
 
 let command_line_args = require('command-line-args');
-let ghpages = require('gh-pages');
 
 let args = command_line_args([
   {
@@ -18,8 +17,10 @@ let args = command_line_args([
 ]);
 
 if (!args.publish) {
-  throw new Error('Usage: node publish_github.js --publish|-p <folder>');
+  throw new Error('Usage: node publish_github.js (--publish|-p) <folder>');
 }
+
+let ghpages = require('gh-pages');
 
 ghpages.publish(args.publish, (err) => {
   if (err) console.error(err);
